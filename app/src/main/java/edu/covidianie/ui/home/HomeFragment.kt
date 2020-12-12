ebd693697dabc188675fc13407595091a061c0d2
SitamMatt <mateuszszewc@op.pl>
2020-12-12 12:25:28 +0100

    migration to androidx

--- app/src/main/java/edu/covidianie/ui/home/HomeFragment.kt
+++ app/src/main/java/edu/covidianie/ui/home/HomeFragment.kt
@@ -5,10 +5,15 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.TextView
-import android.support.v4.app.Fragment
-import android.arch.lifecycle.Observer
-import android.arch.lifecycle.ViewModelProvider
+import androidx.fragment.app.Fragment
+import androidx.lifecycle.Observer
+import androidx.lifecycle.ViewModelProvider
+import androidx.lifecycle.lifecycleScope
 import edu.covidianie.R
+import org.jsoup.Jsoup
+import org.jsoup.nodes.Document
+import org.jsoup.select.Elements
+
 
 class HomeFragment : Fragment() {
 
@@ -26,6 +31,16 @@ class HomeFragment : Fragment() {
         homeViewModel.text.observe(viewLifecycleOwner, Observer {
             textView.text = it
         })
+
+//        viewLifecycleOwner.lifecycleScope.apply {
+//            val doc: Document = Jsoup.connect("https://en.wikipedia.org/").get()
+////            log(doc.title())
+//            val newsHeadlines: Elements = doc.select("#mp-itn b a")
+//            for (headline in newsHeadlines) {
+////                log("%s\n\t%s",
+////                        headline.attr("title"), headline.absUrl("href"))
+//            }
+//        }
         return root
     }
 }
